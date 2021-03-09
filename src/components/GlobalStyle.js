5e32fa5caf743d4df43887357d3e550a153a3a6d
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-09 20:21:18 +0530

    x scroll fix

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,67 +3,68 @@ import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-body {
-    background: #1b1b1b;
-    font-family: 'Roboto', sans-serif;
-}
-
-button {
-    font-weight: bold;
-    font-size: 1.1rem;
-    cursor: pointer;
-    padding: 1rem 2rem;
-    border: 3px solid #23d997;
-    background: transparent;
-    color: #fff;
-    transition: all .5s ease;
-    font-family: 'Roboto', sans-serif;
-
-    &:focus {
-        outline: none;
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
 
-    &:hover {
-        background: #23d997;
+    body {
+        background: #1b1b1b;
+        font-family: 'Roboto', sans-serif;
+        overflow-x: hidden;
+    }
+
+    button {
+        font-weight: bold;
+        font-size: 1.1rem;
+        cursor: pointer;
+        padding: 1rem 2rem;
+        border: 3px solid #23d997;
+        background: transparent;
         color: #fff;
+        transition: all .5s ease;
+        font-family: 'Roboto', sans-serif;
+
+        &:focus {
+            outline: none;
+        }
+
+        &:hover {
+            background: #23d997;
+            color: #fff;
+        }
     }
-}
 
-h2 {
-    font-weight: lighter;
-    font-size: 3rem;
-}
+    h2 {
+        font-weight: lighter;
+        font-size: 3rem;
+    }
 
-h3 {
-    color: white;
-}
+    h3 {
+        color: white;
+    }
 
-h4 {
-    font-size: 2rem;
-    font-weight: bold;
-}
+    h4 {
+        font-size: 2rem;
+        font-weight: bold;
+    }
 
-a {
-    font-size: 1.1rem;
-}
+    a {
+        font-size: 1.1rem;
+    }
 
-span {
-    font-weight: bold;
-    color: #23d997;
-}
+    span {
+        font-weight: bold;
+        color: #23d997;
+    }
 
-p {
-    color: #ccc;
-    padding: 3rem 0;
-    font-size: 1.4rem;
-    line-height: 150%;
-}
+    p {
+        color: #ccc;
+        padding: 3rem 0;
+        font-size: 1.4rem;
+        line-height: 150%;
+    }
 
 `
 
