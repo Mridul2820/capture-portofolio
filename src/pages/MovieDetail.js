a5d15e21de580ee521bc5f30ed5d3192156774d3
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-09 21:02:44 +0530

    Update MovieDetail.js

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -14,7 +14,7 @@ const MovieDetail = () => {
     const history = useHistory();
     const url = history.location.pathname;
 
-    const [movies, setMovies] = useState(MovieState)
+    const [movies] = useState(MovieState)
     const [movie, setMovie] = useState(null)
 
     useEffect(() => {
